014c91378b2159c13ad5a3844de8cf21158e3e98
lambdaji <jzygle@gmail.com>
2018-01-09 20:56:09 +0800

    commit
    deepfm class

--- deep_ctr/DeepFM.py
+++ deep_ctr/DeepFM.py
@@ -36,7 +36,141 @@ tf.app.flags.DEFINE_string("tr_dir", '/ceph_ai/data/', "train data dir")
 tf.app.flags.DEFINE_string("model_dir", '/ceph_ai/model/', "model check point dir")
 tf.app.flags.DEFINE_string("task_type", 'train', "task type {train, predict, evaluate}")
 
-def get_feature_map(if_str):
+class DeepFM(object):
+    """docstring fo DeepFM."""
+    def __init__(self, arg):
+        super(DeepFM, self).__init__()
+        self.fw_b = None     #bias
+        self.fw_l = None     #fm linear weights
+        self.fw_v = None     #embedding v
+
+    def init_graph(self):
+        """Init a tensorflow Graph containing: input data, variables, model, loss, optimizer"""
+        graph = tf.Graph()
+        with graph.as_default():
+            #------model weights------
+	        self.fw_b = tf.get_variable(name='fm_bias', shape=[1], initializer=tf.constant_initializer(0.0))
+	        self.fw_l = tf.get_variable(name='fm_w', shape=[FLAGS.feature_size], initializer=tf.glorot_normal_initializer())
+	        self.fw_v = tf.get_variable(name='fm_v', shape=[FLAGS.feature_size,FLAGS.embedding_size], initializer=tf.glorot_normal_initializer())
+
+            y = model_fn()
+
+    #1 1:0.5 2:0.03519 3:1 4:0.02567 7:0.03708 8:0.01705 9:0.06296 10:0.18185 11:0.02497 12:1 14:0.02565 15:0.03267 17:0.0247 18:0.03158 20:1 22:1 23:0.13169 24:0.02933 27:0.18159 31:0.0177 34:0.02888 38:1 51:1 63:1 132:1 164:1 236:1
+    def input_fn(filenames, batch_size=32, num_epochs=1, perform_shuffle=False):
+        print('Parsing', filenames)
+        def decode_libsvm(line):
+            #columns = tf.decode_csv(value, record_defaults=CSV_COLUMN_DEFAULTS)
+            #features = dict(zip(CSV_COLUMNS, columns))
+            #labels = features.pop(LABEL_COLUMN)
+            columns = tf.string_split([line], ' ')
+            labels = tf.string_to_number(columns.values[0], out_type=tf.int32)
+            splits = tf.string_split(columns.values[1:], ':')
+            id_vals = tf.reshape(splits.values,splits.dense_shape)
+            feat_ids, feat_vals = tf.split(id_vals,num_or_size_splits=2,axis=1)
+            feat_ids = tf.string_to_number(feat_ids, out_type=tf.int32)
+            feat_vals = tf.string_to_number(feat_vals, out_type=tf.float32)
+            #for i in range(splits.dense_shape.eval()[0]):
+            #    feat_ids.append(tf.string_to_number(splits.values[2*i], out_type=tf.int32))
+            #    feat_vals.append(tf.string_to_number(splits.values[2*i+1]))
+            #return tf.reshape(feat_ids,shape=[-1,FLAGS.field_size]), tf.reshape(feat_vals,shape=[-1,FLAGS.field_size]), labels
+            return feat_ids, feat_vals, labels
+
+        # Extract lines from input files using the Dataset API, can pass one filename or filename list
+        dataset = tf.data.TextLineDataset(filenames).map(decode_libsvm, num_parallel_calls=10).prefetch(500000)    # multi-thread pre-process then prefetch
+
+        # Randomizes input using a window of 256 elements (read into memory)
+        if perform_shuffle:
+            dataset = dataset.shuffle(buffer_size=256)
+
+        # epochs from blending together.
+        dataset = dataset.repeat(num_epochs)
+        dataset = dataset.batch(batch_size) # Batch size to use
+
+        return dataset.make_one_shot_iterator()
+        #iterator = dataset.make_one_shot_iterator()
+        #batch_ids, batch_vals, batch_labels = iterator.get_next()
+        #return tf.reshape(batch_ids,shape=[-1,FLAGS.field_size]), tf.reshape(batch_vals,shape=[-1,FLAGS.field_size]), batch_labels
+        #return tf.reshape(batch_ids,shape=[-1,FLAGS.field_size]), batch_vals, batch_labels
+
+
+    def model_fn(self, feat_ids, feat_vals, labels):
+        """bulid f(x)"""
+        #------FM first-order------
+        with tf.variable_scope("first-order"):
+        	feat_wgts = tf.nn.embedding_lookup(wl, feat_ids) # None * F * 1
+        	fm_w = tf.reduce_sum(tf.mulitply(feat_wgts,feat_vals),1)
+
+        #------FM second-order------
+        with tf.variable_scope("second-order"):
+        	embeddings = tf.nn.embedding_lookup(wv, feat_ids) # None * F * K
+            feat_vals = tf.reshape(feat_vals, shape=[-1, FLAGS.field_size, 1])
+        	embeddings = tf.multiply(embeddings,feat_vals) #vij*xi
+        	sum_square = tf.square(tf.reduce_sum(embeddings,1))
+        	square_sum = tf.reduce_sum(tf.square(embeddings),1)
+        	fm_v = 0.5*tf.reduce_sum(tf.subtract(sum_square, square_sum),1)	# None * 1
+
+        #------mlp------
+        with tf.variable_scope("deep"):
+        	deep_inputs = tf.reshape(embeddings,shape=[-1,FLAGS.field_size*FLAGS.embedding_size]) # None * (F*K)
+        	layers = map(int,FLAGS.deep_layers.split(','))
+        	for i in range(len(layers)):
+        		deep_inputs = tf.contrib.layers.fully_connected(inputs=deep_inputs, num_outputs=layers[i], \
+        						weights_regularizer=tf.contrib.layers.l2_regularizer(FLAGS.l2_reg), scope='mlp%d' % i)
+            deep_out = tf.contrib.layers.fully_connected(inputs=deep_inputs, num_outputs=1, activation_fn=tf.identity, \
+        						weights_regularizer=tf.contrib.layers.l2_regularizer(FLAGS.l2_reg), scope='deep_out')
+        	#sig_wgts = tf.get_variable(name='sigmoid_weights', shape=[layers[-1]], initializer=tf.glorot_normal_initializer())
+        	#sig_bias = tf.get_variable(name='sigmoid_bias', shape=[1], initializer=tf.constant_initializer(0.0))
+        	#deep_out = tf.nn.xw_plus_b(deep_inputs,sig_wgts,sig_bias,name='deep_out')
+
+        #------sigmoid------
+        with tf.variable_scope("deepfm"):
+        	bias = wb * tf.ones_like(self.train_labels)  # None * 1
+        	y = tf.sigmoid(bias + fm_w + fm_v + deep_out)
+
+        #------calc auc------
+    	auc = tf.contrib.metrics.streaming_auc(y,label)
+
+    	#------bulid loss------
+    	#lossL2 = tf.add_n([ tf.nn.l2_loss(v) for v in vars if 'bias' not in v.name ]) * 0.001
+    	loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y, labels=label)) + \
+                            FLAGS.l2_reg * tf.nn.l2_loss(wl) + \
+                            FLAGS.l2_reg * tf.nn.l2_loss(wv) #+ \ FLAGS.l2_reg * tf.nn.l2_loss(sig_wgts)
+
+    	#------bulid optimizer------
+    	if FLAGS.optimizer_type == 'Adam':
+            optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-8).minimize(loss)
+        elif FLAGS.optimizer_type == 'Adagrad':
+            optimizer = tf.train.AdagradOptimizer(learning_rate=FLAGS.learning_rate, initial_accumulator_value=1e-8).minimize(loss)
+        elif FLAGS.optimizer_type == 'Momentum':
+            optimizer = tf.train.MomentumOptimizer(learning_rate=FLAGS.learning_rate, momentum=0.95).minimize(loss)
+        elif FLAGS.optimizer_type == 'ftrl':
+            optimizer = tf.train.FtrlOptimizer(FLAGS.learning_rate).minimize(loss)
+
+        #return loss, auc, optimizer
+
+    def train():
+        tr_begin = time()
+	    iterator = input_fn(FLAGS.tr_dir, True, FLAGS.batch_size, FLAGS.epochs)
+  	    while True:
+    	try:
+			batch_ids, batch_vals, batch_labels = iterator.get_next()
+            loss = model_fn(batch_ids, batch_vals, batch_labels)
+            #feature to map to id
+			feed_dict = {feat_ids: batch_ids.eval(), feat_vals: batch_vals.eval(), labels: batch_labels}
+        	loss, auc = sess.run((loss, auc), feed_dict=feed_dict)
+      		print("global_steps=%d	tr_loss=%lf	tr_auc=%lf".format(tf.global_steps, loss, auc))
+
+			if tf.global_steps % 10000 == 0:
+				va_loss, va_auc = evaluate()
+				print("global_steps=%d	va_loss=%lf	va_auc=%lf".format(tf.global_steps, va_loss, va_auc))
+    	except tf.errors.OutOfRangeError:
+      		break
+
+    def evaluate(self):
+        pass
+
+    def infer(self):
+        pass
 
 
 '''
@@ -48,7 +182,7 @@ with graph.as_default():
 	#------bulid feature------
 
 	feat_ids  = tf.placeholder(tf.int32,shape=[None,None],name='feat_ids') #batch_size*F
-	feat_vals = tf.placeholder(tf.float,shape=[None,None],name='feat_vals')
+	feat_vals = tf.placeholder(tf.float32,shape=[None,None],name='feat_vals')
 	label = tf.placeholder(tf.float32, shape=[None,1], name='label')  # None * 1
 
 	#------model weights------
@@ -62,11 +196,12 @@ with graph.as_default():
 		fm_w = tf.reduce_sum(tf.mulitply(feat_wgts,feat_vals),1)
 
 	with tf.variable_scope("second-order"):
-		embeddings = tf.nn.embedding_lookup(wv, feat_ids) # None * F * 1
-		embeddings = tf.mulitply(embeddings,feat_vals) #vij*xi
+		embeddings = tf.nn.embedding_lookup(wv, feat_ids) # None * F * K
+        feat_vals = tf.reshape(feat_vals, shape=[-1, FLAGS.field_size, 1])
+		embeddings = tf.multiply(embeddings,feat_vals) #vij*xi
 		sum_square = tf.square(tf.reduce_sum(embeddings,1))
 		square_sum = tf.reduce_sum(tf.square(embeddings),1)
-		fm_v = 0.5*tf.reduce_sum(tf.subtract(sum_square - square_sum),1)	# None * 1
+		fm_v = 0.5*tf.reduce_sum(tf.subtract(sum_square, square_sum),1)	# None * 1
 
 	with tf.variable_scope("deep"):
 		deep_inputs = tf.reshape(embeddings,shape=[-1,FLAGS.field_size*FLAGS.embedding_size]) # None * (F*K)
@@ -84,25 +219,10 @@ with graph.as_default():
 		bias = wb * tf.ones_like(self.train_labels)  # None * 1
 		y = tf.sigmoid(bias + fm_w + fm_v + deep_out)
 
-	#------bulid loss------
-	#lossL2 = tf.add_n([ tf.nn.l2_loss(v) for v in vars if 'bias' not in v.name ]) * 0.001
-	loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y, labels=label)) + \
-                        FLAGS.l2_reg * tf.nn.l2_loss(wl) + \
-                        FLAGS.l2_reg * tf.nn.l2_loss(wv) #+ \ FLAGS.l2_reg * tf.nn.l2_loss(sig_wgts)
 
-	#------bulid optimizer------
-	if FLAGS.optimizer_type == 'Adam':
-        optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-8).minimize(loss)
-    elif FLAGS.optimizer_type == 'Adagrad':
-        optimizer = tf.train.AdagradOptimizer(learning_rate=FLAGS.learning_rate, initial_accumulator_value=1e-8).minimize(loss)
-    elif FLAGS.optimizer_type == 'Momentum':
-        optimizer = tf.train.MomentumOptimizer(learning_rate=FLAGS.learning_rate, momentum=0.95).minimize(loss)
-    elif FLAGS.optimizer_type == 'ftrl':
-        optimizer = tf.train.FtrlOptimizer(FLAGS.learning_rate).minimize(loss)
 
 	#------summary for TensorBoard-------
     tf.summary.scalar('tr_loss', loss)
-	auc = tf.contrib.metrics.streaming_auc(y,label)
 	tf.summary.scalar('tr_auc', auc)
     #tf.summary.histogram('fm_w_hist', wl)
     #tf.summary.histogram('fm_v_hist', wv)
@@ -131,70 +251,19 @@ CSV_COLUMN_CTR_DEFAULTS = [[0.0] for i in range(17)]
 XGB_COLUMN_DEFAULTS = [[0] for i in range(xgb_trees)]
 CSV_COLUMN_DEFAULTS = CSV_COLUMN_DEFAULTS + CSV_COLUMN_CTR_DEFAULTS + XGB_COLUMN_DEFAULTS
 
-#0,ADR,5.1,PHONE,OOV,3,小说,阅读,21,Sat,0,0.00008,0.00332,0.01201,0.01147,0.02055,0.0218,0.01952,0.03548,0.00202,0.00371,0.0006,0.00422,0.00036,0.00002,0.16667,0.00026,0.00026,1046,1057,1050,1053,1090,1080,1077,1084,1045,1028,977,851,1091,796,1147,946,905,702,141,699,718,650,430,776,340,475,572,565,516,459,416,451,390,365,573,337,530,521,311,421,372,530,707,571,472,673,392,529,402,208,541,407,276,565,458,387,567,264,246,494,247,692,656,534,301,565,642,557,321,656,387,325,535,404,250,636,313,232,625,680,500,438,625,624,280,435,365,523,353,185,297,507,377,381,120,202,329,730,517,332
-#1 1:0.5 2:0.03519 3:1 4:0.02567 7:0.03708 8:0.01705 9:0.06296 10:0.18185 11:0.02497 12:1 14:0.02565 15:0.03267 17:0.0247 18:0.03158 20:1 22:1 23:0.13169 24:0.02933 27:0.18159 31:0.0177 34:0.02888 38:1 51:1 63:1 132:1 164:1 236:1
-
-def input_fn(filenames, batch_size=32, num_epochs=1, perform_shuffle=False):
-    print('Parsing', filenames)
-    def parse_csv(line):
-        #columns = tf.decode_csv(value, record_defaults=CSV_COLUMN_DEFAULTS)
-        #features = dict(zip(CSV_COLUMNS, columns))
-        #labels = features.pop(LABEL_COLUMN)
-        columns = tf.string_split([line], ' ')
-        labels = tf.string_to_number(columns.values[0], out_type=tf.int32)
-        feat_ids = []
-        feat_vals = []
-        for col in columns.values[1:].eval():
-            splits = tf.string_split([col], ':')
-            feat_ids.append(tf.string_to_number(splits.values[0], out_type=tf.int32))
-            feat_vals.append(tf.string_to_number(splits.values[1]))
-        return feat_ids, feat_vals, labels
-
-    # Extract lines from input files using the Dataset API.
-    dataset = tf.data.TextLineDataset(filenames) # can pass one filename or filename list
-
-    # multi-thread pre-process then prefetch
-    dataset = dataset.map(parse_csv, num_parallel_calls=10).prefetch(500000)
-
-    # We call repeat after shuffling, rather than before, to prevent separate
-    if perform_shuffle:
-       # Randomizes input using a window of 256 elements (read into memory)
-       dataset = dataset.shuffle(buffer_size=256)
-
-    # epochs from blending together.
-    dataset = dataset.repeat(num_epochs)
-    dataset = dataset.batch(batch_size) # Batch size to use
-
-    #return dataset.make_one_shot_iterator()
-    iterator = dataset.make_one_shot_iterator()
-    batch_ids, batch_vals, batch_labels = iterator.get_next()
-    return batch_ids, batch_vals, batch_labels
 
 
-def train():
-	tr_begin = time()
-	iterator = input_fn(FLAGS.tr_dir, True, FLAGS.batch_size, FLAGS.epochs)
-  	while True:
-    	try:
-			batch_features, batch_labels = iterator.get_next()
-            #feature to map to id
-			feed_dict = {feat_ids: batch_features.keys(), feat_vals: batch_features.values(), labels: batch_labels}
-        	loss, auc = sess.run((loss, auc), feed_dict=feed_dict)
-      		print("global_steps=%d	tr_loss=%lf	tr_auc=%lf".format(tf.global_steps, loss, auc))
 
-			if tf.global_steps % 10000 == 0:
-				va_loss, va_auc = evaluate()
-				print("global_steps=%d	va_loss=%lf	va_auc=%lf".format(tf.global_steps, va_loss, va_auc))
-    	except tf.errors.OutOfRangeError:
-      		break
+
+
 
 def evaluate():
 	pass
 
-def predict():
+def infer():
 	pass
 
-
+def
 if __name__ == '__main__':
     # Data loading
 
