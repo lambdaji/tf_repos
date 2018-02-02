35d02feb5572bf0e422a3bb03d616e17f2f48665
lambdaji <jzygle@gmail.com>
2018-02-02 17:12:38 +0800

    update deepfm
    run

--- deep_ctr/DeepFM.py
+++ deep_ctr/DeepFM.py
@@ -3,7 +3,9 @@
 """
 #1 Input pipline using Dataset high level API, Support parallel and prefetch reading
 #2 Train pipline using Coustom Estimator
-#3 Support distincted training
+#3 Support distincted training by TF_CONFIG
+
+by lambdaji
 """
 #from __future__ import absolute_import
 #from __future__ import division
@@ -20,6 +22,7 @@ from time import time
 #from multiprocessing import Process
 
 #import math
+import random
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -27,43 +30,44 @@ import tensorflow as tf
 #################### CMD Arguments ####################
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_integer("dist_mode", 0, "distribuion mode {0-loacal, 1-single_dist, 2-multi_dist}")
-tf.app.flags.DEFINE_string("ps_hosts", "", "Comma-separated list of hostname:port pairs")
-tf.app.flags.DEFINE_string("worker_hosts", "", "Comma-separated list of hostname:port pairs")
-tf.app.flags.DEFINE_string("job_name", "", "One of 'ps', 'worker'")
+tf.app.flags.DEFINE_string("ps_hosts", '', "Comma-separated list of hostname:port pairs")
+tf.app.flags.DEFINE_string("worker_hosts", '', "Comma-separated list of hostname:port pairs")
+tf.app.flags.DEFINE_string("job_name", '', "One of 'ps', 'worker'")
 tf.app.flags.DEFINE_integer("task_index", 0, "Index of task within the job")
 tf.app.flags.DEFINE_integer("num_threads", 16, "Number of threads")
-tf.app.flags.DEFINE_integer("feature_size", 16, "Number of features")
-tf.app.flags.DEFINE_integer("field_size", 16, "Number of fields")
-tf.app.flags.DEFINE_integer("embedding_size", 16, "Embedding size")
-tf.app.flags.DEFINE_integer("epochs", 16, "Number of epochs")
-tf.app.flags.DEFINE_integer("batch_size", 16, "Number of batch size")
-tf.app.flags.DEFINE_integer("log_steps", 10000, "save summary every steps")
-tf.app.flags.DEFINE_float("learning_rate", 0.001, "learning rate")
-tf.app.flags.DEFINE_float("l2_reg", 0.01, "L2 regularization")
+tf.app.flags.DEFINE_integer("feature_size", 0, "Number of features")
+tf.app.flags.DEFINE_integer("field_size", 0, "Number of fields")
+tf.app.flags.DEFINE_integer("embedding_size", 32, "Embedding size")
+tf.app.flags.DEFINE_integer("num_epochs", 10, "Number of epochs")
+tf.app.flags.DEFINE_integer("batch_size", 64, "Number of batch size")
+tf.app.flags.DEFINE_integer("log_steps", 1000, "save summary every steps")
+tf.app.flags.DEFINE_float("learning_rate", 0.0005, "learning rate")
+tf.app.flags.DEFINE_float("l2_reg", 0.0001, "L2 regularization")
 tf.app.flags.DEFINE_string("loss_type", 'log_loss', "loss type {square_loss, log_loss}")
 tf.app.flags.DEFINE_string("optimizer", 'Adam', "optimizer type {Adam, Adagrad, GD, Momentum}")
-tf.app.flags.DEFINE_string("deep_layers", '128,64,32', "deep layers")
-tf.app.flags.DEFINE_string("data_dir", '/ceph_ai/data/', "data dir")
+tf.app.flags.DEFINE_string("deep_layers", '256,128,64', "deep layers")
+tf.app.flags.DEFINE_string("data_dir", '', "data dir")
 tf.app.flags.DEFINE_string("dt_dir", '', "data dt partition")
-tf.app.flags.DEFINE_string("model_dir", '/ceph_ai/model/', "model check point dir")
-tf.app.flags.DEFINE_string("servable_model_dir","/ceph_ai/model/servable_model/","export servable model for TensorFlow Serving")
+tf.app.flags.DEFINE_string("model_dir", '', "model check point dir")
+tf.app.flags.DEFINE_string("servable_model_dir", '', "export servable model for TensorFlow Serving")
 tf.app.flags.DEFINE_string("task_type", 'train', "task type {train, infer, eval, export}")
 tf.app.flags.DEFINE_boolean("clear_existing_model", False, "clear existing model or not")
 
 #1 1:0.5 2:0.03519 3:1 4:0.02567 7:0.03708 8:0.01705 9:0.06296 10:0.18185 11:0.02497 12:1 14:0.02565 15:0.03267 17:0.0247 18:0.03158 20:1 22:1 23:0.13169 24:0.02933 27:0.18159 31:0.0177 34:0.02888 38:1 51:1 63:1 132:1 164:1 236:1
-def input_fn(self, ilenames, batch_size=32, num_epochs=1, perform_shuffle=False):
+def input_fn(filenames, batch_size=32, num_epochs=1, perform_shuffle=False):
     print('Parsing', filenames)
     def decode_libsvm(line):
         #columns = tf.decode_csv(value, record_defaults=CSV_COLUMN_DEFAULTS)
         #features = dict(zip(CSV_COLUMNS, columns))
         #labels = features.pop(LABEL_COLUMN)
         columns = tf.string_split([line], ' ')
-        labels = tf.string_to_number(columns.values[0], out_type=tf.int32)
+        labels = tf.string_to_number(columns.values[0], out_type=tf.float32)
         splits = tf.string_split(columns.values[1:], ':')
         id_vals = tf.reshape(splits.values,splits.dense_shape)
         feat_ids, feat_vals = tf.split(id_vals,num_or_size_splits=2,axis=1)
         feat_ids = tf.string_to_number(feat_ids, out_type=tf.int32)
         feat_vals = tf.string_to_number(feat_vals, out_type=tf.float32)
+        #feat_ids = tf.reshape(feat_ids,shape=[-1,FLAGS.field_size])
         #for i in range(splits.dense_shape.eval()[0]):
         #    feat_ids.append(tf.string_to_number(splits.values[2*i], out_type=tf.int32))
         #    feat_vals.append(tf.string_to_number(splits.values[2*i+1]))
@@ -95,6 +99,7 @@ def model_fn(features, labels, mode, params):
     embedding_size = params["embedding_size"]
     l2_reg = params["l2_reg"]
     learning_rate = params["learning_rate"]
+    #optimizer = params["optimizer"]
     layers = map(int, params["deep_layers"].split(','))
 
     #------bulid weights------
@@ -104,7 +109,9 @@ def model_fn(features, labels, mode, params):
 
     #------build feaure-------
     feat_ids  = features['feat_ids']
+    feat_ids = tf.reshape(feat_ids,shape=[-1,field_size])
     feat_vals = features['feat_vals']
+    feat_vals = tf.reshape(feat_vals,shape=[-1,field_size])
 
     #------build f(x)------
     with tf.variable_scope("first-order"):
@@ -113,7 +120,7 @@ def model_fn(features, labels, mode, params):
 
     with tf.variable_scope("second-order"):
         embeddings = tf.nn.embedding_lookup(FM_V, feat_ids) # None * F * K
-        #feat_vals = tf.reshape(feat_vals, shape=[-1, field_size, 1])
+        feat_vals = tf.reshape(feat_vals, shape=[-1, field_size, 1])
         embeddings = tf.multiply(embeddings, feat_vals) #vij*xi
         sum_square = tf.square(tf.reduce_sum(embeddings,1))
         square_sum = tf.reduce_sum(tf.square(embeddings),1)
@@ -121,26 +128,31 @@ def model_fn(features, labels, mode, params):
 
     with tf.variable_scope("deep"):
         deep_inputs = tf.reshape(embeddings,shape=[-1,field_size*embedding_size]) # None * (F*K)
-        layers = map(int,deep_layers.split(','))
         for i in range(len(layers)):
             deep_inputs = tf.contrib.layers.fully_connected(inputs=deep_inputs, num_outputs=layers[i], \
             weights_regularizer=tf.contrib.layers.l2_regularizer(l2_reg), scope='mlp%d' % i)
 
         y_deep = tf.contrib.layers.fully_connected(inputs=deep_inputs, num_outputs=1, activation_fn=tf.identity, \
             weights_regularizer=tf.contrib.layers.l2_regularizer(l2_reg), scope='deep_out')
+        y_d = tf.reshape(y_deep,shape=[-1])
         #sig_wgts = tf.get_variable(name='sigmoid_weights', shape=[layers[-1]], initializer=tf.glorot_normal_initializer())
         #sig_bias = tf.get_variable(name='sigmoid_bias', shape=[1], initializer=tf.constant_initializer(0.0))
         #deep_out = tf.nn.xw_plus_b(deep_inputs,sig_wgts,sig_bias,name='deep_out')
 
     with tf.variable_scope("deepfm"):
-        y_bias = FM_B * tf.ones_like(labels, dtype=tf.float32)  # None * 1
-        y = tf.sigmoid(y_bias + y_w + y_v + y_deep)
+        #y_bias = FM_B * tf.ones_like(labels, dtype=tf.float32)  # None * 1  warning;这里不能用label，否则调用predict/export函数会出错，train/evaluate正常；初步判断estimator做了优化，用不到label是不传
+        y_bias = FM_B * tf.ones_like(y_d, dtype=tf.float32)     # None * 1
+        y = y_bias + y_w + y_v + y_d
+        pred = tf.sigmoid(y)
 
+    predictions={"prob": pred}
+    export_outputs = {tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY: tf.estimator.export.PredictOutput(predictions)}
     # Provide an estimator spec for `ModeKeys.PREDICT`
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(
                 mode=mode,
-                predictions={"prob": y})
+                predictions=predictions,
+                export_outputs=export_outputs)
 
     #------bulid loss------
     loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=y, labels=labels)) + \
@@ -149,114 +161,42 @@ def model_fn(features, labels, mode, params):
 
     # Provide an estimator spec for `ModeKeys.EVAL`
     eval_metric_ops = {
-        "auc": tf.metrics.auc(labels, y)
-        "logloss": loss
+        "auc": tf.metrics.auc(labels, pred)
     }
     if mode == tf.estimator.ModeKeys.EVAL:
         return tf.estimator.EstimatorSpec(
                 mode=mode,
-                predictions={"prob": y},
+                predictions=predictions,
                 loss=loss,
                 eval_metric_ops=eval_metric_ops)
 
     #------bulid optimizer------
-    if optimizer_type == 'Adam':
+    if FLAGS.optimizer == 'Adam':
         optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9, beta2=0.999, epsilon=1e-8)
-    elif optimizer_type == 'Adagrad':
+    elif FLAGS.optimizer == 'Adagrad':
         optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate, initial_accumulator_value=1e-8)
-    elif optimizer_type == 'Momentum':
+    elif FLAGS.optimizer == 'Momentum':
         optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.95)
-    elif optimizer_type == 'ftrl':
+    elif FLAGS.optimizer == 'ftrl':
         optimizer = tf.train.FtrlOptimizer(learning_rate)
 
     train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
 
     # Provide an estimator spec for `ModeKeys.TRAIN` modes
     if mode == tf.estimator.ModeKeys.TRAIN:
-        return tf.estimator.EstimatorSpec(mode, y, loss, train_op)
+        return tf.estimator.EstimatorSpec(
+                mode=mode,
+                predictions=predictions,
+                loss=loss,
+                train_op=train_op)
 
-def train():
-    tr_begin = time()
-    #iterator = input_fn(tr_dir, batch_size, epochs, True)
-    while True:
-        try:
-            feat_ids, feat_vals, labels = iterator.get_next()
-            #feed_dict = {feat_ids: batch_ids.eval(), feat_vals: batch_vals.eval(), labels: batch_labels}
-            loss, auc, _ = sess.run([loss, auc, optimizer])
-            print("global_steps=%d	tr_loss=%lf	tr_auc=%lf".format(tf.global_steps, loss, auc))
-
-            if tf.global_steps % eval_steps == 0:
-                va_loss, va_auc = evaluate()
-                print("global_steps=%d	va_loss=%lf	va_auc=%lf".format(tf.global_steps, va_loss, va_auc))
-        except tf.errors.OutOfRangeError:
-            print 'Done training -- epoch limit reached'
-            break
-
-    print("Training task time elapsed = %lf".format(time() - tr_begin))
-
-    #Infer task
-    infer()
-
-def evaluate(self):
-    #tr_begin = time()
-    iterator = input_fn(va_dir, batch_size)
-    va_loss = []
-    va_auc = []
-    while True:
-        try:
-            feat_ids, feat_vals, labels = iterator.get_next()
-            #feed_dict = {feat_ids: batch_ids.eval(), feat_vals: batch_vals.eval(), labels: batch_labels}
-            loss, auc, _ = sess.run([loss, auc])
-            va_loss.extend(loss)
-            va_auc.extend(auc)
-        except tf.errors.OutOfRangeError:
-            break
-
-    return np.mean(va_loss), np.mean(va_auc)
-
-def infer(self):
-    tr_begin = time()
-    iterator = input_fn(va_dir, batch_size)
-    with open(pred_file, "w") as fo:
-        while True:
-            try:
-                feat_ids, feat_vals, labels = iterator.get_next()
-                #feed_dict = {feat_ids: batch_ids.eval(), feat_vals: batch_vals.eval(), labels: batch_labels}
-                y = sess.run([y])
-                fo.write("%f\n" % (y))
-            except tf.errors.OutOfRangeError:
-                print 'Done infering -- epoch limit reached'
-                break
-
-    print("Infering task time elapsed = %lf".format(time() - tr_begin))
-
-def export_model():
-    # 将训练好的模型保存在当前的文件夹下
-    builder = tf.saved_model.builder.SavedModelBuilder(join("./model_name", MODEL_VERSION))
-    inputs = {
-        "x_wide": tf.saved_model.utils.build_tensor_info(x_wide),
-        "x_deep": tf.saved_model.utils.build_tensor_info(x_deep)
-        }
-    output = {
-        "output": tf.saved_model.utils.build_tensor_info(prediction)
-        }
-    prediction_signature = tf.saved_model.signature_def_utils.build_signature_def(
-        inputs=inputs,
-        outputs=output,
-        method_name=tf.saved_model.signature_constants.PREDICT_METHOD_NAME
-        )
-
-    builder.add_meta_graph_and_variables(
-        sess,
-        [tf.saved_model.tag_constants.SERVING],
-        signature_def_map = {
-            tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY: prediction_signature
-            }
-        )
-    builder.save()
-
-def load_model():
-    pass
+    # Provide an estimator spec for `ModeKeys.EVAL` and `ModeKeys.TRAIN` modes.
+    #return tf.estimator.EstimatorSpec(
+    #        mode=mode,
+    #        loss=loss,
+    #        train_op=train_op,
+    #        predictions={"prob": pred},
+    #        eval_metric_ops=eval_metric_ops)
 
 def set_dist_env():
     if FLAGS.dist_mode == 1:        # 本地分布式测试模式1 chief, 1 ps, 1 evaluator
@@ -298,31 +238,32 @@ def set_dist_env():
         if job_name == "worker" and task_index > 1:
             task_index -= 2
 
-         tf_config = {
-             'cluster': {'chief': chief_hosts, 'worker': worker_hosts, 'ps': ps_hosts},
-             'task': {'type': job_name, 'index': task_index }
-         }
-         print(json.dumps(tf_config))
-         os.environ['TF_CONFIG'] = json.dumps(tf_config)
+        tf_config = {
+            'cluster': {'chief': chief_hosts, 'worker': worker_hosts, 'ps': ps_hosts},
+            'task': {'type': job_name, 'index': task_index }
+        }
+        print(json.dumps(tf_config))
+        os.environ['TF_CONFIG'] = json.dumps(tf_config)
 
 def main(_):
-    tr_files = glob.glob("%s/tr.libsvm" % FLAGS.data_dir)
+    tr_files = glob.glob("%s/tr*libsvm" % FLAGS.data_dir)
     random.shuffle(tr_files)
     print("tr_files:", tr_files)
-    va_files = glob.glob("%s/va.libsvm" % FLAGS.data_dir)
+    va_files = glob.glob("%s/va*libsvm" % FLAGS.data_dir)
     print("va_files:", va_files)
-    te_files = glob.glob("%s/te.libsvm" % FLAGS.data_dir)
+    te_files = glob.glob("%s/te*libsvm" % FLAGS.data_dir)
     print("te_files:", te_files)
-
-    set_dist_env()
 
     if FLAGS.clear_existing_model:
         try:
-            shutil.rmtree(model_dir)
+            shutil.rmtree(FLAGS.model_dir)
         except Exception as e:
             print(e, "at clear_existing_model")
         else:
-            print("existing model cleaned at %s" % model_dir)
+            print("existing model cleaned at %s" % FLAGS.model_dir)
+
+    set_dist_env()
+
     model_params = {
         "field_size": FLAGS.field_size,
         "feature_size": FLAGS.feature_size,
@@ -333,39 +274,45 @@ def main(_):
     }
     config = tf.estimator.RunConfig().replace(session_config = tf.ConfigProto(device_count={'GPU':0, 'CPU':FLAGS.num_threads}),
             log_step_count_steps=FLAGS.log_steps, save_summary_steps=FLAGS.log_steps)
-    DeepFM = tf.estimator.Estimator(model_fn=model_fn, model_dir=model_dir, params=model_params, config=config)
+    DeepFM = tf.estimator.Estimator(model_fn=model_fn, model_dir=FLAGS.model_dir, params=model_params, config=config)
 
-    iBegin = time()
     if FLAGS.task_type == 'train':
         train_spec = tf.estimator.TrainSpec(input_fn=lambda: input_fn(tr_files, num_epochs=FLAGS.num_epochs, batch_size=FLAGS.batch_size))
         eval_spec = tf.estimator.EvalSpec(input_fn=lambda: input_fn(va_files, num_epochs=1, batch_size=FLAGS.batch_size), steps=None, start_delay_secs=1000, throttle_secs=1200)
         tf.estimator.train_and_evaluate(DeepFM, train_spec, eval_spec)
     elif FLAGS.task_type == 'eval':
-        DeepFM.evaluate(input_fn=lambda: input_fn(eval_files, num_epochs=1, batch_size=FLAGS.batch_size))
+        DeepFM.evaluate(input_fn=lambda: input_fn(va_files, num_epochs=1, batch_size=FLAGS.batch_size))
     elif FLAGS.task_type == 'infer':
-        preds = DeepFM.predict(input_fn=lambda: input_fn(te_file, num_epochs=1, batch_size=FLAGS.batch_size), predict_keys="prob")
+        preds = DeepFM.predict(input_fn=lambda: input_fn(te_files, num_epochs=1, batch_size=FLAGS.batch_size), predict_keys="prob")
         with open(FLAGS.data_dir+"/pred.txt", "w") as fo:
             for prob in preds:
-                fo.write("%f\n" % (prob['prob'][1]))
+                fo.write("%f\n" % (prob['prob']))
     elif FLAGS.task_type == 'export':
-        #?
-        feature_spec = tf.feature_column.make_parse_example_spec(feature_columns)
-        serving_input_receiver_fn = tf.estimator.export.build_parsing_serving_input_receiver_fn(feature_spec)
-        estimator.export_savedmodel(FLAGS.servable_model_dir, serving_input_receiver_fn)
-    print("Time elapsed: %lf".format(time() - iBegin))
+        #feature_spec = tf.feature_column.make_parse_example_spec(feature_columns)
+        #feature_spec = {
+        #    'feat_ids': tf.FixedLenFeature(dtype=tf.int64, shape=[None, FLAGS.field_size]),
+        #    'feat_vals': tf.FixedLenFeature(dtype=tf.float32, shape=[None, FLAGS.field_size])
+        #}
+        #serving_input_receiver_fn = tf.estimator.export.build_parsing_serving_input_receiver_fn(feature_spec)
+        feature_spec = {
+            'feat_ids': tf.placeholder(dtype=tf.int64, shape=[None, FLAGS.field_size], name='feat_ids'),
+            'feat_vals': tf.placeholder(dtype=tf.float32, shape=[None, FLAGS.field_size], name='feat_vals')
+        }
+        serving_input_receiver_fn = tf.estimator.export.build_raw_serving_input_receiver_fn(feature_spec)
+        DeepFM.export_savedmodel(FLAGS.servable_model_dir, serving_input_receiver_fn)
 
 if __name__ == "__main__":
     #------check Arguments------
-    if FLAGS.dt_dir:
-        FLAGS.dt_dir = date() ??
+    if FLAGS.dt_dir == "":
+        FLAGS.dt_dir = (date.today() + timedelta(-1)).strftime('%Y%m%d')
     FLAGS.model_dir = FLAGS.model_dir + FLAGS.dt_dir
-    FLAGS.data_dir  = FLAGS.data_dir + FLAGS.dt_dir
+    #FLAGS.data_dir  = FLAGS.data_dir + FLAGS.dt_dir
 
     print('task_type ', FLAGS.task_type)
     print('model_dir ', FLAGS.model_dir)
     print('data_dir ', FLAGS.data_dir)
     print('dt_dir ', FLAGS.dt_dir)
-    print('epochs ', FLAGS.epochs)
+    print('num_epochs ', FLAGS.num_epochs)
     print('feature_size ', FLAGS.feature_size)
     print('field_size ', FLAGS.field_size)
     print('embedding_size ', FLAGS.embedding_size)
